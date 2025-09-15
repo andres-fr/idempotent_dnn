f33e1a06c4febd5c2c8e5e2181d2d8ad6886426c
andres-fr <fr_andres@msn.com>
2025-09-15 19:06:26 +0200

    new MLP successfully overfitting FMNIST

--- 02_idempotent_fmnist_reconstruction.py
+++ 02_idempotent_fmnist_reconstruction.py
@@ -84,7 +84,33 @@ def initialize_module(model, distr=torch.nn.init.xavier_uniform_, bias_val=0):
         elif "bias" in pname:
             p.data[:] = bias_val
         else:
+            # distr(p)
             raise NotImplementedError(f"Unknown weight: {pname}")
+
+
+# class IdempotentMLP(torch.nn.Module):
+#     """
+#     This one works well on FMNIST trained for 15 epochs with BCELoss and
+#     Adam(lr=1e-3, wd=1e-5). 0.25 loss is already decent
+#     """
+
+#     def __init__(self, hidden_dims=350, rank=150, activation=torch.nn.Tanh):
+#         """ """
+#         super().__init__()
+#         self.down = torch.nn.Linear(784, hidden_dims)
+#         self.h1 = IdempotentLinear(hidden_dims, rank, bias=True, saturation=2)
+#         self.a1 = activation()
+#         self.up = torch.nn.Linear(hidden_dims, 784)
+
+#     def forward(self, x):
+#         """ """
+#         dsts = []
+#         x = self.down(x)
+#         x, dst1 = self.h1(x)
+#         dsts.append(dst1)
+#         x = self.a1(x)
+#         x = self.up(x)
+#         return x, dsts
 
 
 class PCA_MLP(torch.nn.Module):
@@ -105,63 +131,85 @@ class PCA_MLP(torch.nn.Module):
         return x, dsts
 
 
-class RegularMLP(torch.nn.Module):
-    """ """
+class MultichanMLP(torch.nn.Module):
+    """
 
-    def __init__(self, hidden_dims=350, activation=torch.nn.LeakyReLU):
+    This MLP autoencoder has a simple, linear encoder and a more complex,
+    feedforward decoder that alternates pixel-layers with channel-layers:
+    * In the first decoder layer, ``hdim`` copies of the ``zdim``-dimensional
+      latent code are created, and for each copy, a linear layer is applied,
+      producing ``chans`` features.
+    * The following channel-layer contains, for each copy, a ``chan*chan``
+      linear layer that acts as channel mixer
+    * The following pixel-layer consists of a linear layer that is applied
+      to each channel idependently, mapping from any number of copies
+      (e.g. ``hdim``) to a different number of copies (e.g. 784).
+
+    Alternating channel mixers with pixel mixers allows to overfit the FMNIST
+    reconstruction task, while keeping a feedforward, linear MLP style and
+    a very simple encoder.
+    """
+
+    def __init__(
+        self,
+        zdim=350,
+        chans=30,
+        xdim=784,
+        hdim=500,
+        with_1b=True,
+        with_2b=True,
+    ):
         """ """
+        self.with_1b, self.with_2b = with_1b, with_2b
         super().__init__()
-        self.down = torch.nn.Linear(784, hidden_dims)
-        self.body = torch.nn.Sequential(
-            activation(),
-            torch.nn.Linear(hidden_dims, hidden_dims),
-            activation(),
-            torch.nn.Linear(hidden_dims, hidden_dims),
-            activation(),
-            # torch.nn.Linear(hidden_dims, hidden_dims),
-            # activation(),
+        self.enc = torch.nn.Sequential(
+            torch.nn.Linear(784, zdim),
         )
-        self.up = torch.nn.Sequential(
-            torch.nn.Linear(hidden_dims, 784 * 3),
-            activation(),
-            torch.nn.Linear(784 * 3, 784 * 2),
-            activation(),
-            torch.nn.Linear(784 * 2, 784),
-        )
+        #
+        self.dec_weight1 = torch.nn.Parameter(torch.randn(500, zdim, chans))
+        self.dec_bias1 = torch.nn.Parameter(torch.randn(500, chans))
+        #
+        if with_1b:
+            self.dec_weight1b = torch.nn.Parameter(
+                torch.randn(500, chans, chans)
+            )
+            self.dec_bias1b = torch.nn.Parameter(torch.randn(500, chans))
+        #
+        self.dec_weight2 = torch.nn.Parameter(torch.randn(784, 500, chans))
+        self.dec_bias2 = torch.nn.Parameter(torch.randn(784, chans))
+        #
+        if with_2b:
+            self.dec_weight2b = torch.nn.Parameter(
+                torch.randn(784, chans, chans)
+            )
+            self.dec_bias2b = torch.nn.Parameter(torch.randn(784, chans))
+        #
+        self.dec_weight3 = torch.nn.Parameter(torch.randn(784, chans))
+        self.dec_bias3 = torch.nn.Parameter(torch.randn(784))
+        #
         initialize_module(self, torch.nn.init.xavier_uniform_, 0)
 
     def forward(self, x):
         """ """
-        dsts = []
-        x = self.down(x)
-        x = self.body(x)
-        x = self.up(x)
-        return x, dsts
-
-
-class IdempotentMLP(torch.nn.Module):
-    """
-    This one works well on FMNIST trained for 15 epochs with BCELoss and
-    Adam(lr=1e-3, wd=1e-5). 0.25 loss is already decent
-    """
-
-    def __init__(self, hidden_dims=350, rank=150, activation=torch.nn.Tanh):
-        """ """
-        super().__init__()
-        self.down = torch.nn.Linear(784, hidden_dims)
-        self.h1 = IdempotentLinear(hidden_dims, rank, bias=True, saturation=2)
-        self.a1 = activation()
-        self.up = torch.nn.Linear(hidden_dims, 784)
-
-    def forward(self, x):
-        """ """
-        dsts = []
-        x = self.down(x)
-        x, dst1 = self.h1(x)
-        dsts.append(dst1)
-        x = self.a1(x)
-        x = self.up(x)
-        return x, dsts
+        x = self.enc(x)  # b, zdim
+        #
+        x = torch.einsum("oic,bi->boc", self.dec_weight1, x) + self.dec_bias1
+        x = F.gelu(x)  # b, 500, c
+        #
+        if self.with_1b:
+            x = torch.einsum("nck,bnc->bnk", self.dec_weight1b, x)
+            x = F.gelu(x + self.dec_bias1b)
+        #
+        x = torch.einsum("oic,bic->boc", self.dec_weight2, x) + self.dec_bias2
+        x = F.gelu(x)  # b, 784, c
+        #
+        if self.with_2b:
+            x = torch.einsum("nck,bnc->bnk", self.dec_weight2b, x)
+            x = F.gelu(x + self.dec_bias2b)
+        #
+        x = torch.einsum("nc,bnc->bn", self.dec_weight3, x) + self.dec_bias3
+        x = F.sigmoid(x)
+        return x, []
 
 
 # ##############################################################################
@@ -171,9 +219,9 @@ if __name__ == "__main__":
     DTYPE = torch.float32
     DEVICE = "cuda"  #  "cuda" if torch.cuda.is_available() else "cpu"
     FMNIST_PATH = os.path.join("datasets", "FashionMNIST")
-    BATCH_SIZE = 150
-    LR, MOMENTUM, WEIGHT_DECAY = 1e-4, 0, 0  #  1e-5, 0.9, 1e-4
-    NUM_EPOCHS = 50
+    BATCH_SIZE = 60  # 30
+    LR, MOMENTUM, WEIGHT_DECAY = 5e-4, 0, 0  #  1e-5, 0.9, 1e-4
+    NUM_EPOCHS = 70
 
     train_ds = torchvision.datasets.FashionMNIST(
         FMNIST_PATH,
@@ -186,7 +234,7 @@ if __name__ == "__main__":
         train_ds, batch_size=BATCH_SIZE, shuffle=True
     )
 
-    model1 = RegularMLP(350).to(DEVICE)
+    model1 = MultichanMLP(350).to(DEVICE)  # RegularMLP(350).to(DEVICE)
     model2 = PCA_MLP(350).to(DEVICE)  # IdempotentMLP(350, rank=150).to(DEVICE)
     opt1 = torch.optim.Adam(
         model1.parameters(), lr=LR, weight_decay=WEIGHT_DECAY
@@ -194,7 +242,10 @@ if __name__ == "__main__":
     opt2 = torch.optim.Adam(
         model2.parameters(), lr=LR, weight_decay=WEIGHT_DECAY
     )
-    loss_fn = torch.nn.BCEWithLogitsLoss()
+    bce_fn = torch.nn.BCEWithLogitsLoss()
+    l1_fn = torch.nn.L1Loss()
+    mse_fn = torch.nn.MSELoss()
+    loss_fn = mse_fn  # bce_fn
     #
     global_step = 0
     for epoch in range(NUM_EPOCHS):
@@ -205,6 +256,7 @@ if __name__ == "__main__":
             #
             preds1, dists = model1(imgs.reshape(BATCH_SIZE, -1))
             preds1 = preds1.reshape(imgs.shape)
+            # loss = bce_fn(preds1, imgs) + 0.1 * l1_fn(preds1, imgs)
             loss = loss_fn(preds1, imgs)
             loss.backward()
             opt1.step()
@@ -219,6 +271,7 @@ if __name__ == "__main__":
             #
             preds2, dists = model2(imgs.reshape(BATCH_SIZE, -1))
             preds2 = preds2.reshape(imgs.shape)
+            # loss = bce_fn(preds2, imgs)  # + 0.1 * l1_fn(preds2, imgs)
             loss = loss_fn(preds2, imgs)
             loss.backward()
             opt2.step()
@@ -234,5 +287,6 @@ if __name__ == "__main__":
             global_step += 1
     # 0.24 BCELoss is good
     # idx = 0; fig, (ax1, ax2, ax3) = plt.subplots(ncols=3); ax1.imshow(imgs[idx, 0].detach().cpu()), ax2.imshow(preds1[idx, 0].sigmoid().detach().cpu()); ax3.imshow(preds2[idx, 0].sigmoid().detach().cpu()); fig.show()
+    # idx = 0; fig, (ax1, ax2, ax3) = plt.subplots(ncols=3); ax1.imshow(imgs[idx, 0].detach().cpu()), ax2.imshow(preds1[idx, 0].detach().cpu()); ax3.imshow(preds2[idx, 0].detach().cpu()); fig.show()
     # idx = 0; fig, (ax1, ax2) = plt.subplots(ncols=2); ax1.imshow(imgs[idx, 0].detach()), ax2.imshow(preds1[idx, 0].sigmoid().detach()); fig.show()
     breakpoint()
